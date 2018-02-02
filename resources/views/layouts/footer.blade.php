97f1eb99851fe0c0336c98755b41f494051f57df
Humeira Diljore <hello@echdee.me>
2018-02-02 22:38:21 +0400

    FRONT - footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,36 @@
 <div class="bg-grey-darker">
     <div class="container mx-auto px-4 py-8">
-        <div class="flex flex-wrap">
-            <div class="w-2/5 my-2 text-grey-lightest">
-                <h4 class="font-serif">Mauritian Space</h4>
-                <p class="font-sans py-2">
+        <div class="flex flex-wrap -mx-4">
+            <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
+                <h4 class="font-serif text-md tracking-wide py-2">Mauritian Space</h4>
+                <p class="font-sans font-light py-1">
                     Mauritian Space curates free tech events happening on the island and showcase open source projects from fellow crafts(wo)men.
                 </p>
             </div>
-            <div class="w-1/5 my-2 text-grey-lightest">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam at commodi consectetur cum cumque doloribus eius et explicabo in, iste laborum mollitia, neque odit quibusdam quidem recusandae suscipit veritatis.</div>
-            <div class="w-1/5 my-2 text-grey-lightest">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dignissimos doloremque dolores libero nam quas suscipit, tempora ut voluptate? Dicta eos labore optio, pariatur porro qui repudiandae sit suscipit voluptatum!</div>
-            <div class="w-1/5 my-2 text-grey-lightest">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dignissimos doloremque dolores libero nam quas suscipit, tempora ut voluptate? Dicta eos labore optio, pariatur porro qui repudiandae sit suscipit voluptatum!</div>
+            <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
+                <h4 class="font-serif text-md tracking-wide py-2">Community Sites</h4>
+                <p class="font-sans font-light py-1">
+                    <a href="http://mscc.mu" class="text-grey-lightest hover:text-grey-lighter no-underline">Mauritius Software Craftsmanship Community </a>
+                </p>             <p class="font-sans font-light py-1">
+                    <a href="http://symfony.mu/" class="text-grey-lightest hover:text-grey-lighter no-underline">Symfony.mu</a>
+                </p>
+                <p class="font-sans font-light py-1">
+                    <a href="http://hackers.mu/" class="text-grey-lightest hover:text-grey-lighter no-underline">Hackers.mu</a>
+                </p>
+                <p class="font-sans font-light py-1">
+                    <a href="http://techin.africa" class="text-grey-lightest hover:text-grey-lighter no-underline">Tech In Africa</a>
+                </p>
+
+            </div>
+            <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
+                <h4 class="font-serif text-md tracking-wide py-2">Colophon</h4>
+                <p class="font-sans font-light py-1">
+                    Built with Laravel, VueJS & Tailwind
+                </p>
+                <p class="font-sans font-light py-1">
+                    Proudly hosted with Digital Ocean
+                </p>
+            </div>
         </div>
     </div>
 </div>
