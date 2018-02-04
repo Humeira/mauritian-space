6376c1991c01885cc7997a613a8f8381fdd44118
Humeira Diljore <hello@echdee.me>
2018-02-04 12:16:59 +0400

    keeping 3 columns in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,21 +8,6 @@
                 </p>
             </div>
             <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
-                <h4 class="font-serif text-md tracking-wide py-2">Community Sites</h4>
-                <p class="font-sans font-light py-1">
-                    <a href="http://mscc.mu" class="text-grey-lightest hover:text-grey-lighter no-underline">Mauritius Software Craftsmanship Community </a>
-                </p>             <p class="font-sans font-light py-1">
-                    <a href="http://symfony.mu/" class="text-grey-lightest hover:text-grey-lighter no-underline">Symfony.mu</a>
-                </p>
-                <p class="font-sans font-light py-1">
-                    <a href="http://hackers.mu/" class="text-grey-lightest hover:text-grey-lighter no-underline">Hackers.mu</a>
-                </p>
-                <p class="font-sans font-light py-1">
-                    <a href="http://techin.africa" class="text-grey-lightest hover:text-grey-lighter no-underline">Tech In Africa</a>
-                </p>
-
-            </div>
-            <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
                 <h4 class="font-serif text-md tracking-wide py-2">Colophon</h4>
                 <p class="font-sans font-light py-1">
                     Built with Laravel, VueJS & Tailwind
@@ -30,6 +15,11 @@
                 <p class="font-sans font-light py-1">
                     Proudly hosted with Digital Ocean
                 </p>
+            </div>
+            <div class="sm:w-1/3 my-2 text-grey-lightest px-4">
+                <h4 class="font-serif text-md tracking-wide py-2">
+                    Subscribe
+                </h4>
             </div>
         </div>
     </div>
