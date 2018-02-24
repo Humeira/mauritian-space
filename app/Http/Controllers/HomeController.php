e0c42fdb0a34b81e43203af82352cfbe444c7bef
Humeira Diljore <hello@echdee.me>
2018-02-24 23:48:52 +0400

    event api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use \App\Api\Event as Event;
+
 
 class HomeController extends Controller
 {
@@ -15,12 +15,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $data = array();
-        $eventList = new Event(env('MEETUP_API_COUNTRY'));
-        $eventList = $eventList->FetchEvents();
-        $data = [
-            "events" => $eventList
-        ];
-        return view('index')->with('data', $data);
+        return view('index');
     }
 }
